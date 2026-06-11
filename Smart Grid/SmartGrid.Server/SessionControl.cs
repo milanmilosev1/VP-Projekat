7aad5827eb02dc99d4f8e03ab88e2c51fddbffa5
milanmilosev1 <milosevmil@gmail.com>
2026-06-11 13:33:06 +0200

    Added delegates and events to the project

--- Smart Grid/SmartGrid.Server/SessionControl.cs
+++ Smart Grid/SmartGrid.Server/SessionControl.cs
@@ -1,7 +1,10 @@
 ﻿using SmartGrid.Common;
 using SmartGrid.Common.Validators;
+using SmartGrid.Server.Events;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
 using System.Linq;
 using System.ServiceModel;
 using System.Xml.Linq;
@@ -16,6 +19,10 @@ namespace SmartGrid.Server
 
         private readonly double _iThreshold;
         private readonly double _vThreshold;
+        private readonly double _averageDeviationThreshold;
+
+        private readonly SmartGridEventHub _events;
+        private readonly SmartGridEventLogger _eventLogger;
 
         private static int _sampleCount = 0;
         private static int _acceptedSampleCount = 0;
@@ -25,15 +32,19 @@ namespace SmartGrid.Server
         {
             try 
             {
-                var doc = XDocument.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
-                var settings = doc.Descendants("appSettings").Descendants("add");
-                _iThreshold = double.Parse(settings.FirstOrDefault(x => x.Attribute("key")?.Value == "I_threshold")?.Attribute("value")?.Value ?? "15.0", System.Globalization.CultureInfo.InvariantCulture);
-                _vThreshold = double.Parse(settings.FirstOrDefault(x => x.Attribute("key")?.Value == "V_threshold")?.Attribute("value")?.Value ?? "230.0", System.Globalization.CultureInfo.InvariantCulture);
+                _iThreshold = double.Parse(ConfigurationManager.AppSettings["I_Threshold"]);
+                _vThreshold = double.Parse(ConfigurationManager.AppSettings["V_Threshold"]);
+                _averageDeviationThreshold = double.Parse(ConfigurationManager.AppSettings["AverageDeviationThreshold"]);
+                _events = new SmartGridEventHub();
+                _eventLogger = new SmartGridEventLogger(_events, ConfigurationManager.AppSettings["EventLogURL"]);
             }
             catch 
             {
                 _iThreshold = 15.0;
                 _vThreshold = 230.0;
+                _averageDeviationThreshold = 0.25;
+                _events = new SmartGridEventHub();
+                _eventLogger = new SmartGridEventLogger(_events, "server_events.log");
             }
         }
 
@@ -47,6 +58,7 @@ namespace SmartGrid.Server
             _meta = metaHeader;
             _sampleMeasurements.Clear();
             _sessionActive = true;
+            _events.RaiseTransferStarted($"Transfer started at {metaHeader.Timestamp:O}.");
 
             return new SessionResponse
             {
@@ -68,20 +80,23 @@ namespace SmartGrid.Server
                 var sampleValidator = new SampleValidator(_iThreshold, _vThreshold);
                 sampleValidator.Validate(sample);
 
+                _events.RaiseSampleReceived(sample, _sampleCount + 1);
 
                 if (_sampleMeasurements.Count > 0)
                 {
                     double avgV = _sampleMeasurements.Average(m => m.Voltage);
                     double avgI = _sampleMeasurements.Average(m => m.Current);
 
-                    if (Math.Abs(sample.Voltage - avgV) > 0.25 * avgV)
+                    if (Math.Abs(sample.Voltage - avgV) > _averageDeviationThreshold * avgV)
                     {
+                        _events.RaiseWarningRaised("VoltageAverageDeviation", GetDirection(sample.Voltage, avgV), sample.Voltage, avgV, _averageDeviationThreshold, sample);
                         _rejectedSampleCount++; 
                         return new SessionResponse { Status = Status.NAK, Progress = Progress.IN_PROGRESS, Message = "Voltage deviates > 25% from average." };
                     }
 
-                    if (Math.Abs(sample.Current - avgI) > 0.25 * avgI)
+                    if (Math.Abs(sample.Current - avgI) > _averageDeviationThreshold * avgI)
                     {
+                        _events.RaiseWarningRaised("CurrentAverageDeviation", GetDirection(sample.Current, avgI), sample.Current, avgI, _averageDeviationThreshold, sample);
                         _rejectedSampleCount++;
                         return new SessionResponse { Status = Status.NAK, Progress = Progress.IN_PROGRESS, Message = "Current deviates > 25% from average." };
                     }
@@ -121,6 +136,7 @@ namespace SmartGrid.Server
                 throw new FaultException<ValidationFault>(new ValidationFault { Reason = "No active session to end." }, new FaultReason("No active session to end."));
 
             _sessionActive = false;
+            _events.RaiseTransferCompleted($"Transfer completed. Processed {_sampleCount} samples.", _sampleCount, _acceptedSampleCount, _rejectedSampleCount);
 
             return new SessionResponse
             {
@@ -128,6 +144,16 @@ namespace SmartGrid.Server
                 Progress = Progress.COMPLETED,
                 Message = $"Session ended successfully. Processed {_sampleCount} samples.\n\nAccepted: {_acceptedSampleCount}\nRejected: {_rejectedSampleCount}\n"
             };
+        }
+
+        private static string GetSetting(IEnumerable<XElement> settings, string key, string defaultValue)
+        {
+            return settings.FirstOrDefault(x => x.Attribute("key")?.Value == key)?.Attribute("value")?.Value ?? defaultValue;
+        }
+
+        private static string GetDirection(double actualValue, double expectedValue)
+        {
+            return actualValue > expectedValue ? "iznad ocekivanog" : "ispod ocekivanog";
         }
     }
 }
